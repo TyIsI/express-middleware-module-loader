ea3550103114233589db50342c766b490e93e785
Ty Eggen <tycho.eggen@gmail.com>
2018-09-28 14:51:07 -0700

    Changed internal hook concept to modules

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ var debug = require('debug')('express-middleware-module-loader');
 var fs = require('fs');
 var path = require('path');
 
-// Hooks Cache
-var hooks_cache = {};
+// Modules Cache
+var modules_cache = {};
 
-// Load all the hooks
-function loadHooksFromDirectory(search_dir) {
+// Load all the modules
+function loadModulesFromDirectory(search_dir) {
     var search_path = path.resolve(search_dir);
 
     debug("Search path:",search_path);
 
-    fs.readdirSync(search_dir).forEach(function (hook_name) {
+    fs.readdirSync(search_dir).forEach(function (module_name) {
 
-        if (fs.statSync(path.join(search_dir, hook_name)).isDirectory()) {
-            debug("Found hook:", hook_name);
+        if (fs.statSync(path.join(search_dir, module_name)).isDirectory()) {
+            debug("Found module:", module_name);
 
-            let hook_dir = path.join(search_path, hook_name);
+            let module_dir = path.join(search_path, module_name);
 
-            debug("Loading from:", hook_dir);
-            hooks_cache[hook_name] = require(hook_dir);
+            debug("Loading from:", module_dir);
+            modules_cache[module_name] = require(module_dir);
 
-            var hook_handle = '/' + hook_name;
+            var module_handle = '/' + module_name;
 
-            debug("Adding hook route:", hook_handle);
-            router.use(hook_handle, hooks_cache[hook_name]);
+            debug("Adding module route:", module_handle);
+            router.use(module_handle, modules_cache[module_name]);
         }
 
     });
 }
 
-var getHooks = function () {
-    return Object.keys(hooks_cache);
+var getModules = function () {
+    return Object.keys(modules_cache);
 }
 
 var loadModule = function (dirs = []) {
@@ -54,8 +54,8 @@ var loadModule = function (dirs = []) {
         throw new Error("Empty dirs argument:\n" + dirs);
 
     dirs.forEach(function (dir) {
-        debug("Loading hooks from directory:", dir);
-        loadHooksFromDirectory(dir);
+        debug("Loading modules from directory:", dir);
+        loadModulesFromDirectory(dir);
     })
 
     return router;
@@ -63,6 +63,6 @@ var loadModule = function (dirs = []) {
 
 module.exports = loadModule;
 
-module.exports.getHooks = getHooks;
+module.exports.getModules = getModules;
 
 exports = module.exports;
